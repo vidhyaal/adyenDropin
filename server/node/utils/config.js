b22ff6547e0a99fcd8486e25afa1d9a0bdee99f6
vidhyaal <vidhyaal@gmail.com>
2019-11-20 20:31:42 +0800

    new API key

--- server/node/utils/config.js
+++ server/node/utils/config.js
@@ -3,7 +3,7 @@
 
 const API_VERSION = 'v50';
 const CHECKOUT_URL = `https://checkout-test.adyen.com/${API_VERSION}`;
-const CHECKOUT_APIKEY='AQE1hmfxJo/OaR1Kw0m/n3Q5qf3Ve55dHZxYTFdTxWq+l3JOk8J4BA+ztqPuS4G6vYkJoj+c4MIQwV1bDb7kfNy1WIxIIkxgBw==-+g+FdHJB1s/8mcg/evWIDkJgGZRqO7hMsodFya1e7f0=-Bg2vV8qEgP65bdMd';
+const CHECKOUT_APIKEY='AQE1hmfxJo/OaR1Kw0m/n3Q5qf3Ve55dHZxYTFdTxWq+l3JOk8J4BA+ztqPuS4G6vYkJoj+c4MIQwV1bDb7kfNy1WIxIIkxgBw==-cpyGy0LKZtWHM3D+Mk38kzhN3F+nlAtza4Rf9jFsNO8=-h4tyr9I25QRa4GEV';
 const MERCHANT_ACCOUNT='SupportRecruitementCOM';
 module.exports = {
     CHECKOUT_APIKEY,
